788eb81e1fafe5ff2de620022d18aee12b8a62ab
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-08-10 16:20:57 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var moment = require("moment");
+// var tinycolor = require("tinycolor2");
+
+console.log("Hello from JavaScript");
+var datetime = moment().startOf("day").fromNow();
+console.log(datetime);
+console.log("testing testing 123");
+console.log("helloooo");
+
+// var color = tinycolor("red");
+// tinycolor(console.log("Hello from JavaScript"));
+// var name = "Bob";
+// var color = "Blue";
+// console.log(`Hello ${name}! Your favorite color is ${color}`);
+
+// class Animal {
+//   constructor(name, type) {
+//     super(name, type);
+
+//     this.name = "Sherlock";
+//     this.type = "Turtoise";
+//   }
+// }
+
+// console.log(`${name} is a ${type}`);
